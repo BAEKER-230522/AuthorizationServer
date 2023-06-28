12e2e1e01b210962ff8fffe1fb226208e1ff498b
PARKPARKWOO <wy9295@naver.com>
2023-06-29 00:26:08 +0900

    setting

---
+++ src/main/java/com/example/authorize/entity/UserCredential.java
@@ -0,0 +1,5 @@
+package com.example.authorize.entity;
+
+public class UserCredential {
+
+}
---
+++ src/main/java/com/example/authorize/service/AuthService.java
@@ -0,0 +1,13 @@
+//package com.example.authorize.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class AuthService {
+//    private final PasswordEncoder passwordEncoder;
+//    private final JwtService jwtService;
+//
+//}
